1e27e50c9e14c0ff2c96184ba9dc394496832278
Vitaly <vitaliken@gmail.com>
2022-02-11 22:23:19 +0300

    add: function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (stack.length === 0) {
+      stack.push(str[i]);
+    } else {
+      for (let z = 0; z < bracketsConfig.length; z++) {
+        if (stack.includes(str[i]) && bracketsConfig[z][0] === bracketsConfig[z][1] && bracketsConfig[z][0] === str[i]) {
+          let counter = 0;
+          for (let y = stack.length; y > 0; y--) {
+
+            if (bracketsConfig[z].indexOf(stack[y - 1]) === 0) {
+              while (counter >= 0) {
+                stack.pop();
+                counter--;
+              }
+            } else {
+              counter++;
+            }
+          }
+        } else if (bracketsConfig[z].indexOf(str[i]) >= 0) {
+          if (bracketsConfig[z].indexOf(str[i]) === 0) {
+            stack.push(str[i]);
+          } else if (bracketsConfig[z].indexOf(str[i]) === 1) {
+            let counter = 0;
+            for (let y = stack.length; y > 0; y--) {
+              if (bracketsConfig[z].indexOf(stack[y - 1]) === 0) {
+                while (counter >= 0) {
+                  stack.pop();
+                  counter--;
+                }
+              } else if (counter === -1) {
+                break;
+              } else {
+                counter++;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return stack.length === 0 ? true : false;
 }
